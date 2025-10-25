2eca7d040cab76fd6074f542d74cdb53f99a868b
edisonmalasan <malasanedison1@gmail.com>
2025-10-25 10:51:44 +0800

    feat(backend): added auth endpoints

---
+++ src/routes/doctorRoutes.php
@@ -0,0 +1,27 @@
+<?php
+require_once '../controllers/AuthController.php';
+
+$route = $_GET['route'] ?? 'index.html';
+$auth = new AuthController();
+
+switch ($route) {
+    case 'login':
+        include '../src/views/auth/login.php';
+        break;
+    case 'register':
+        include '../src/views/auth/register.php';
+        break;
+    case 'doLogin':
+        $auth->login($_POST);
+        break;
+    case 'doRegister':
+        $auth->register($_POST);
+        break;
+
+    // Protected routes
+
+
+
+    default: include './pages/index.html';
+    break;
+}
