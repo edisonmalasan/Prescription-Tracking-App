df573af6c3b384d2b425e02fed24817b1a933b2b
Elizabeth-Eshunna <145834081+Elizabeth-Eshunna@users.noreply.github.com>
2025-11-04 08:10:27 +0800

    fixed pathing issue

--- src/views/pharmacy/Sales.php
+++ src/views/pharmacy/Sales.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sales Management</title>
-    <link rel="stylesheet" href="../../public/assets/css/pharmacy.css">
+    <link rel="stylesheet" href="../../../public/assets/css/pharmacy.css">
 </head>
 <body>
     <div class="container">
